6f1d4865123c3f1dc3fdc0c810e50cc82fb565f6
esameth <46979903+esameth@users.noreply.github.com>
2020-06-25 17:37:42 -0400

    Create utils.py

---
+++ tools/utils.py
@@ -0,0 +1,23 @@
+import os
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-input', type=str, help="folder containing data set", default=os.getcwd())
+    parser.add_argument('-output', type=str, help="folder to output data", default=os.getcwd())
+    parser.add_argument("-c", "--classes", type=int, help="number of classes", required=True)
+    return parser
+
+def process_folder(args):
+    def check_input():
+        if not os.path.exists(args.input + "/Species.tsv"):
+            print(f'{args.input} does not contain a {file} file. Input a valid directory', file=sys.stderr)
+            sys.exit(1)
+        return args.input
+
+    def check_output():
+        if not os.path.isdir(args.output):
+            os.makedirs(args.output)
+        return args.output
+
+    return check_input(), check_output()
