9873d188941b476fe8e48fb441a7cdd533610f24
cissoucr <cissoucr@github.com>
2021-04-30 01:57:23 +0000

    last version test.py

--- nn/models/test.py
+++ nn/models/test.py
@@ -1,20 +1,37 @@
 import tensorflow as tf
+from .utils import load_json_file
 
+@tf.function
 # Define one test step
-def test_step(self, inputs):
+def test_step_training(self, inputs):
+    # in testing and training mode the labels are actual labels (integers),
+    # in inference mode the labels are read ids
     reads, labels = inputs
     predictions = self._model(reads, training=False)
-    # Get true and predicted labels
-#    pred_classes = tf.keras.backend.argmax(predictions)
-#    t_classes = tf.keras.backend.argmax(labels)
-#    proto_tensor_t_classes = tf.make_tensor_proto(t_classes)
-#    proto_tensor_pred_classes = tf.make_tensor_proto(pred_classes)
-#    self.true_classes += tf.make_ndarray(proto_tensor_t_classes).tolist()
-#    self.predicted_classes += tf.make_ndarray(proto_tensor_pred_classes).tolist()
-
+    # update test accuracy for each taxonomic rank
     self.test_accuracy.update_state(labels, predictions)
-    self.test_loss.update_state(self.loss_object(labels, predictions))
+    # get test loss
+    loss = self.loss_object(labels, predictions)
+    # update test loss
+    self.test_loss.update_state(loss)
 
+@tf.function
+def test_step_testing(self, inputs):
+    # in inference mode the labels are read ids and in testing mode the labels
+    # are integers assigned to the genomes in the testing set (different from integers mapping classes in training set)
+    reads, labels = inputs
+    predictions = self._model(reads, training=False)
+    pred_classes = tf.keras.backend.argmax(predictions)
+    self.predicted_classes.append(pred_classes.numpy())
+    self.predictions.append(predictions.numpy())
+    self.read_ids.append(labels.numpy())
+
+@tf.function
 def distributed_test_epoch(self, strategy):
-    for one_batch in self.loaders['val']:
-        strategy.run(test_step, args=(self, one_batch,))
+    functions_dict = {'testing': test_step_testing, 'training': test_step_training,
+                      'inference': test_step_testing}
+    num_test_batches = 0.0
+    for one_batch in self.loaders['test']:
+        strategy.run(functions_dict[self.hparams.mode], args=(self, one_batch,))
+        num_test_batches += 1
+    return num_test_batches
