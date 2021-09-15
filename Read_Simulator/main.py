b6fe288207273e10eaad4ff706ed012bf3ffc72e
Cecile Cres <cecilecres@76.244.20.172.s.wireless.uri.edu>
2021-09-15 14:03:22 -0400

    modify function name when generating processes

--- Read_Simulator/main.py
+++ Read_Simulator/main.py
@@ -27,7 +27,7 @@ def main():
     # generate reads for each species in parallel
     with mp.Manager() as manager:
         # create new processes
-        processes = [mp.Process(target=generate_dataset, args=(args, species, label, needed_iterations)) for label, species in args.label_dict.items()]
+        processes = [mp.Process(target=mutate_genomes, args=(args, species, label, needed_iterations)) for label, species in args.label_dict.items()]
         for p in processes:
             p.start()
         for p in processes:
