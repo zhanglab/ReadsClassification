611f7a87e901a6fe4ddc89d4a3df5fa5f65cc328
Cecile Cres <cecilecres@39.241.20.172.s.wireless.uri.edu>
2021-10-21 11:59:55 -0400

    adapt code to a single node

--- tools/val_train_sets_analysis.py
+++ tools/val_train_sets_analysis.py
@@ -17,19 +17,10 @@ def get_reads(fq_files_loc):
     print(fq_files_loc, len(dataset))
     return dataset
 
-def split_data(filename, size):
-    print(filename)
-    with open(filename, 'r') as f:
-        content = f.readlines()
-        chunk_size = len(content)//size + 1
-        chunks = [content[i:i+chunk_size] for i in range(0, len(content), chunk_size)]
-        print(f'number of chunks: {len(chunks)} - size of chunks: {chunk_size}')
-    return chunks
-
-def parse_linclust(clusters, training_set, validation_set, outfile):
-    for i in clusters:
-        ref = i.rstrip().split('\t')[0]
-        read = i.rstrip().split('\t')[1]
+def parse_linclust(linclust_subset, training_set, validation_set, outfile):
+    for line in linclust_subset:
+        ref = line.rstrip().split('\t')[0]
+        read = line.rstrip().split('\t')[1]
         if ref in training_set and read in validation_set:
             outfile.write(f'{ref}\t{training_set[ref]}\t{read}\t{validation_set[read]}\n')
         elif ref in validaiton_set and read in training_set:
@@ -39,7 +30,6 @@ def parse_linclust(clusters, training_set, validation_set, outfile):
 
 def main():
     input_dir = sys.argv[1]
-    linclust_subset = sys.argv[2]
     # create a communicator consisting of all the processors
     comm = MPI.COMM_WORLD
     # get the number of processors
@@ -51,12 +41,9 @@ def main():
         # create dictionary for storing reads in training and validaiton sets
         training_set = get_reads(os.path.join(input_dir, 'training_data_cov_7x'))
         validation_set = get_reads(os.path.join(input_dir, 'validation_data_cov_7x'))
-        # load and split subset file of linclust output into N lists (N = number of processes)
-        linclust_chunks = split_data(os.path.join(input_dir, linclust_subset), size)
     else:
         training_set = None
         validation_set = None
-        linclust_chunks = None
 
     # create output file
     outfilename = '-'.join(['linclust_out_subset',linclust_subset.rstrip().split('-')[-1], 'rank', rank])
@@ -66,12 +53,12 @@ def main():
     training_set = comm.bcast(training_set, root=0)
     validation_set = comm.bcast(validation_set, root=0)
 
-    # scatter chunks of linclust output to other processes
-    linclust_chunks = comm.scatter(linclust_chunks, root=0)
+    # load linclust output subset
+    linclust_subset = open(os.path.join(input_dir, f'linclust-subset-{rank}'), 'r')
+    print(f'{rank} - {linclust-subset}')
 
     # parse linclust output
-    parse_linclust(linclust_chunks, training_set, validation_set, outfile)
-
+    parse_linclust(linclust_subset, training_set, validation_set, outfile)
 
 
 if __name__ == "__main__":
