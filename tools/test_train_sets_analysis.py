60c8e99697e85eb1470819367981b137eb8da93f
Cecile Cres <cecilecres@159.8.20.172.s.wireless.uri.edu>
2021-10-26 10:58:46 -0400

    create directory to store average mash distances files

--- tools/test_train_sets_analysis.py
+++ tools/test_train_sets_analysis.py
@@ -8,25 +8,30 @@ def get_genomes(filename):
         genomes = [i.rstrip().split('\t')[0] for i in content]
     return genomes
 
-def get_avg_distance(test_genome, train_genomes, mash_dir):
+def get_avg_distance(test_genome, train_genomes, mash_dir, output_path):
     avg_msh_dist = float()
     for train_g in train_genomes:
         with open(os.path.join(mash_dir, f'{test_genome}-{train_g}-mash-dist'), 'r') as f:
             for line in f:
                 avg_msh_dist += float(line.rstrip().split('\t')[2])
     print(f'{test_genome}\t{round(float(avg_msh_dist)/len(train_genomes), 5)}')
-    with open(os.path.join(mash_dir, f'{test_genome}-avg-mash-dist'), 'w') as f:
+    with open(os.path.join(output_path, f'{test_genome}-avg-mash-dist'), 'w') as f:
         f.write(f'{test_genome}\t{round(float(avg_msh_dist)/len(train_genomes), 5)}\n')
 
 def main():
     mash_dir = sys.argv[1]
-    list_train_genomes = sys.argv[2]
-    list_test_genomes = sys.argv[3]
+    list_train_genomes = sys.argv[3]
+    list_test_genomes = sys.argv[4]
     # get training and testing genomes
     train_genomes = get_genomes(list_train_genomes)
     test_genomes = get_genomes(list_test_genomes)
+    print(f'number of processes: {len(test_genomes)}')
+    # create directory to store mash average distances
+    output_path = os.path.join(mash_dir, 'mash-avg-dist')
+    if not os.path.isdir(output_path):
+        os.makedirs(output_path)
     # get average mash distance for each testing genome
-    processes = [mp.Process(target=get_avg_distance, args=(test_g, train_genomes, mash_dir)) for test_g in test_genomes]
+    processes = [mp.Process(target=get_avg_distance, args=(test_g, train_genomes, mash_dir, output_path)) for test_g in test_genomes]
     for p in processes:
         p.start()
     for p in processes:
