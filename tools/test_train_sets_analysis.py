5e18c653819541407f8265027309707341a4ad35
Cecile Cres <cecilecres@159.8.20.172.s.wireless.uri.edu>
2021-11-02 10:55:56 -0400

    compute average mash distance for genomes within the same taxon

--- tools/test_train_sets_analysis.py
+++ tools/test_train_sets_analysis.py
@@ -1,6 +1,19 @@
 import sys
 import os
 import multiprocess as mp
+import pandas as pd
+
+def get_taxonomy(gtdb_info, test_genomes, train_genomes):
+    # load gtdb information
+    gtdb_df = pd.read_csv(gtdb_info, delimiter='\t', low_memory=False)
+    # get species in database
+    taxonomy = [i.split(';')[-1].split('__')[1] for i in list(gtdb_df['gtdb_taxonomy'])]
+    # get genome accession ids
+    accession_id_list = [i[3:] for i in list(gtdb_df.accession)]
+    # get taxonomy
+    genomes_taxonomy = {i: taxonomy[i][::-1] if i in test_genomes or if i in train_genomes}
+    print(f'{len(taxonomy)} - {len(accession_id_list)} - {len(genomes_taxonomy)} - {len(test_genomes)+len(train_genomes)}')
+    return genomes_taxonomy
 
 def get_genomes(filename):
     with open(filename, 'r') as f:
@@ -8,7 +21,10 @@ def get_genomes(filename):
         genomes = [i.rstrip().split('\t')[0] for i in content]
     return genomes
 
-def get_avg_distance(test_genome, train_genomes, mash_dir, output_path):
+def get_avg_distance(test_genome, list_train_genomes, mash_dir, output_path, genomes_taxonomy):
+    # get train genomes with same species as test genome
+    train_genomes = [i for i in list_train_genomes if genomes_taxonomy[i] == genomes_taxonomy[test_genome]]
+    print(f'Number of train genomes associated with test genome {test_genome}: {len(train_genomes)} - species: {genomes_taxonomy[test_genome]}')
     avg_msh_dist = float()
     for train_g in train_genomes:
         with open(os.path.join(mash_dir, f'{test_genome}-{train_g}-mash-dist'), 'r') as f:
@@ -17,23 +33,29 @@ def get_avg_distance(test_genome, train_genomes, mash_dir, output_path):
     print(f'{mp.current_process()}\t{test_genome}\t{round(float(avg_msh_dist)/len(train_genomes), 5)}')
     with open(os.path.join(output_path, f'{test_genome}-avg-mash-dist'), 'w') as f:
         f.write(f'{test_genome}\t{round(float(avg_msh_dist)/len(train_genomes), 5)}\n')
+        f.write(f'Species\t{genomes_taxonomy[test_genome]}\n')
+        for train_g in train_genomes:
+            f.write(f'Training genome\t{train_g}\n')
 
 def main():
     mash_dir = sys.argv[1]
     job_id = int(sys.argv[2])
     list_train_genomes = sys.argv[3]
     list_test_genomes = sys.argv[4]
+    gtdb_info = sys.argv[5]
     # get training and testing genomes
-    train_genomes = get_genomes(list_train_genomes)
+    # train_genomes = get_genomes(list_train_genomes)
     test_genomes = get_genomes(list_test_genomes)
+    train_genomes = get_genomes(list_train_genomes)
     print(f'number of processes: {len(test_genomes)}')
+    genomes_taxonomy = get_taxonomy(gtdb_info, test_genomes, train_genomes)
     # create directory to store mash average distances
-    output_path = os.path.join(mash_dir, 'mash-avg-dist')
+    output_path = os.path.join(mash_dir, 'mash-avg-dist-species')
     if job_id == 0:
         if not os.path.isdir(output_path):
             os.makedirs(output_path)
     # get average mash distance for each testing genome
-    processes = [mp.Process(target=get_avg_distance, args=(test_g, train_genomes, mash_dir, output_path)) for test_g in test_genomes]
+    processes = [mp.Process(target=get_avg_distance, args=(test_g, train_genomes, mash_dir, output_path, genomes_taxonomy)) for test_g in test_genomes]
     for p in processes:
         p.start()
     for p in processes:
