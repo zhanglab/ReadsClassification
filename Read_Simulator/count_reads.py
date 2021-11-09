4fc572497f4790dd20667c6b5c38cf59ecf89389
Cecile Cres <cecilecres@Ceciles-Air.fios-router.home>
2021-11-09 06:58:10 -0500

    create script to count reads in dataset

---
+++ Read_Simulator/count_reads.py
@@ -0,0 +1,24 @@
+import os
+
+def count_reads(input_path, type_reads):
+    list_files = glob.glob(os.path.join(input_path, f'*-{type_reads}-num-reads'))
+
+    total_num_reads = 0
+
+    for nr_file in list_files:
+        with open(nr_file, 'r') as f:
+            content = f.readlines()
+            total_num_reads += int(content[0].rstrip().split('\t')[1])
+
+    with open(os.path.join(input_path, 'total-num-reads'), 'a') as f:
+        f.write(f'{type_reads}\t{total_num_reads}\n')
+
+def main():
+    input_path = sys.argv[1]
+    count_reads(input_path, 'train')
+    count_reads(input_path, 'val')
+    count_reads(input_path, 'test')
+
+
+if __name__ == "__main__":
+    main()
