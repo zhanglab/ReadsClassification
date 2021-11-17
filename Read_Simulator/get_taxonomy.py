037c823db4c596c2f01148c420eac9d8716235ab
Cecile Cres <cecilecres@Ceciles-Air.fios-router.home>
2021-11-17 14:40:08 -0500

    create dictionaries mapping labels at different taxonomic levels

---
+++ Read_Simulator/get_taxonomy.py
@@ -0,0 +1,32 @@
+from utils import get_gtdb_info
+import json
+import sys
+import os
+
+def main():
+    input_dir = sys.argv[1]
+    gtdb_path = sys.argv[2]
+    # load dictionary mapping labels to species
+    f = open(os.path.join(input_dir, 'class_mapping.json'))
+    class_mapping = json.load(f)
+    # get gtdb taxonomy
+    _, _, _, _, gtdb_taxonomy = get_gtdb_info(gtdb_info_path)
+    ranks = ['genus', 'family', 'order', 'class']
+    for i in range(len(ranks)):
+        # get taxa
+        list_taxa = []
+        species_labels = []
+        for j in range(len(class_mapping)):
+            if class_mapping[str(j)] in gtdb_taxonomy:
+                list_taxa.append(gtdb_taxonomy[class_mapping[str(j)]][i].split('__')[1])
+                species_labels.append(j)
+        # create dictionary mapping each taxon at a taxonomic level to a label
+        label_dict = dict(zip(list(range(len(list_taxa))), list_taxa))
+        rev_label_dict = {value: key for key, value in label_dict.items()}
+        with open(os.path.join(args.input_path, f'{ranks[i]}_mapping.json'), "w") as f:
+            json.dump(label_dict, f)
+        # create dictionary mapping labels at the species level to labels at given rank
+        rank_species_dict = {str(k): rev_label_dict[list_taxa[k]] for k in range(len(species_labels))}
+
+if __name__ == "__main__":
+    main()
