c576021d470a5aa6ff3067b8217a771628401148
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2021-12-30 19:48:18 +0100

    add read ids to tfrecords

--- tools/train_tfrecords.py
+++ tools/train_tfrecords.py
@@ -15,27 +15,34 @@ def wrap_read(value):
 def wrap_label(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
+def wrap_ID(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-def convert2tfrecord(args):
-    """
-    Converts reads to tfrecord, and saves to output file
-    """
+def create_tfrecords(args):
+    """ Converts reads to tfrecord, and saves to output file """
+    outfile = open(os.path.join(['/'.join(os.path.abspath(args.input_fastq).split('/')[0:-1], 'read_ids.tsv']))
     with tf.compat.v1.python_io.TFRecordWriter(args.output_tfrec) as writer:
+        # define read ID number
+        read_id_prefix = ''.join([os.path.abspath(args.input_fastq).split('/')[-2].split('-')[1], args.input_fastq.split('-'
         with open(args.input_fastq) as handle:
-            for rec in SeqIO.parse(handle, 'fastq'):
+            for count, rec in enumerate(SeqIO.parse(handle, 'fastq')):
                 read = str(rec.seq)
                 read_id = rec.id
+                read_id_int = int(f'{read_id_prefix}{count}')
+                outfile.write(f'{read_id_int}\t{read_id}\n')
                 label = int(read_id.split('|')[1])
                 kmer_array = seq2kmer(read, args.k_value, args.dict_kmers, args.kmer_vector_length)
                 data = \
                     {
                         'read': wrap_read(kmer_array),
-                        'label': wrap_label(label)
+                        'label': wrap_label(label),
+                        'read_id': wrap_ID(read_id_int)
                     }
                 feature = tf.train.Features(feature=data)
                 example = tf.train.Example(features=feature)
                 serialized = example.SerializeToString()
                 writer.write(serialized)
+    outfile.close()
 
 def main():
 
@@ -51,8 +58,7 @@ def main():
     # get dictionary mapping kmers to indexes
     args.dict_kmers = vocab_dict(args.vocab)
 
-    convert2tfrecord(args)
+    create_tfrecords(args)
 
 if __name__ == "__main__":
     main()
-
