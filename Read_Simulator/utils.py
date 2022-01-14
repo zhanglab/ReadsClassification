21069d48969e4e735146f50d06efffd6c9bea5a4
Cecile Cres <cecilecres@Ceciles-Air.fios-router.home>
2022-01-13 20:49:03 -0500

    added functions to count reads

--- Read_Simulator/utils.py
+++ Read_Simulator/utils.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import glob
 import gzip
 import json
 import pandas as pd
@@ -160,3 +162,33 @@ def get_tetra_nt_fqcy(TETRA_nt, sequence):
         tetra_nt = sequence[i:i+4]
         TETRA_nt[tetra_nt] += 1
     return
+
+def count_reads_train_val(input_path, set_info):
+    list_files = glob.glob(os.path.join(input_path, f'*-train-val-reads'))
+    total_train_num_reads = 0
+    total_val_num_reads = 0
+    for nr_file in list_files:
+        with open(nr_file, 'r') as f:
+            content = f.readlines()
+            total_train_num_reads += int(content[-2].rstrip().split('\t')[1])
+            total_val_num_reads += int(content[-1].rstrip().split('\t')[1])
+            print(int(content[-2].rstrip().split('\t')[1]), int(content[-1].rstrip().split('\t')[1]))
+
+    with open(os.path.join(input_path, 'total-num-reads'), 'a') as f:
+        f.write(f'train\t{total_train_num_reads}\nval\t{total_val_num_reads}\n')
+
+    set_info['train'][0] = total_train_num_reads
+    set_info['val'][0] = total_val_num_reads
+
+def count_reads_test(input_path, set_info):
+    list_files = glob.glob(os.path.join(input_path, f'*-test-reads'))
+    total_test_num_reads = 0
+    for nr_file in list_files:
+        with open(nr_file, 'r') as f:
+            content = f.readlines()
+            total_test_num_reads += int(content[-1].rstrip().split('\t')[1])
+
+    with open(os.path.join(input_path, 'total-num-reads'), 'a') as f:
+        f.write(f'test\t{total_test_num_reads}\n')
+
+    set_info['test'][0] = total_test_num_reads
