b16aecb15dc143febb9b9a8353fc4727fc801377
Cecile Cres <cecilecres@Ceciles-Air.fios-router.home>
2022-01-21 15:16:11 -0500

    added code

--- read_classifier.py
+++ read_classifier.py
@@ -13,8 +13,8 @@ import glob
 import datetime
 import numpy as np
 import math
+from collections import defaultdict
 from models import AlexNet
-from summarize import get_metrics, ROCcurve
 import argparse
 
 # disable eager execution
@@ -96,8 +96,6 @@ def main():
     parser.add_argument('--output_dir', type=str, help='directory to store results', required=True)
     parser.add_argument('--epoch', type=int, help='epoch of checkpoint')
     parser.add_argument('--batch_size', type=int, help='batch size per gpu', default=512)
-    parser.add_argument('--num_reads', type=int, help='number of reads in dataset', required=True)
-    parser.add_argument('--num_reads_in_tfrec', type=int, help='number of reads per tfrecords file', default=1000000)
     parser.add_argument('--model', type=str, help='path to directory containing saved model')
     parser.add_argument('--ckpt', type=str, help='path to directory containing checkpoint file', required=('--epoch' and '--checkpoint' in sys.argv))
     args = parser.parse_args()
@@ -125,10 +123,11 @@ def main():
     opt = tf.keras.optimizers.Adam(init_lr)
     opt = keras.mixed_precision.LossScaleOptimizer(opt)
 
-    # create output directory
-    output_dir = os.path.join(args.output_dir, f'testing-{args.set_type}-set')
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir)
+    if hvd.rank() == 0:
+        # create output directories
+        if not os.path.isdir(output_dir):
+            for i in range(NUM_CLASSES):
+                os.makedirs(os.path.join(output_dir, f'bin-{i}'))
 
     # load model
     if args.ckpt is not None:
@@ -145,27 +144,24 @@ def main():
     #        print(f'latest ckpt: {latest_ckpt}')
     #        model.load_weights(os.path.join(input_dir, f'run-{run_num}', f'ckpts/ckpts-{epoch}'))
 
-    # get list of testing tfrecords
+    # get list of testing tfrecords and number of reads per tfrecords
     test_files = sorted(glob.glob(os.path.join(args.tfrecords, f'{args.set_type}*.tfrec')))
     test_idx_files = sorted(glob.glob(os.path.join(args.dali_idx, f'{args.set_type}*.idx')))
+    num_reads_files = sorted(glob.glob(os.path.join(args.tfrecords, '*-read_count.tsv')))
+    read_ids_files = sorted(glob.glob(os.path.join(args.tfrecords, '*-read_ids.tsv')))
 
     # split tfrecords between gpus
     test_files_per_gpu = math.ceil(len(test_files)/hvd.size())
     gpu_test_files = test_files[hvd.rank()*test_files_per_gpu:(hvd.rank()+1)*test_files_per_gpu]
     gpu_test_idx_files = test_idx_files[hvd.rank()*test_files_per_gpu:(hvd.rank()+1)*test_files_per_gpu]
+    gpu_num_reads_files = num_reads_files[hvd.rank()*test_files_per_gpu:(hvd.rank()+1)*test_files_per_gpu]
+    gpu_read_ids_files = read_ids_files[hvd.rank()*test_files_per_gpu:(hvd.rank()+1)*test_files_per_gpu]
 
-    # create empty confusion matrix with rows = true classes and columns = predicted classes
-    cm = np.zeros((NUM_CLASSES, NUM_CLASSES))
-    # record the number of correct and incorrect predictions
-    num_correct_pred = 0
-    num_incorrect_pred = 0
-    num_reads_tested = 0
-    # create summary file
-    f_prob = open(os.path.join(output_dir, f'probabilities-{hvd.rank()}.tsv'), 'w')
     for i in range(len(gpu_test_files)):
         # get number of reads in test file
-        num_reads = args.num_reads - (len(test_files)*1000000) if i == len(test_files) - 1 else 1000000
-        num_reads_tested += num_reads
+        with open(os.path.join(args.tfrecords, gpu_num_reads_files[i]), 'r') as f:
+            num_reads = int(f.readline()[0])
+        num_reads_classified += num_reads
         # compute number of required steps to iterate over entire test file
         test_steps = math.ceil(num_reads/(args.batch_size))
 
@@ -177,64 +173,55 @@ def main():
 
         # create empty arrays to store the predicted and true values
         all_predictions = tf.zeros([args.batch_size, NUM_CLASSES], dtype=tf.dtypes.float32, name=None)
-        all_labels = [tf.zeros([args.batch_size], dtype=tf.dtypes.float32, name=None)]
+        all_read_ids = [tf.zeros([args.batch_size], dtype=tf.dtypes.float32, name=None)]
 
-        for batch, (reads, labels) in enumerate(test_input.take(test_steps), 1):
-            batch_predictions = testing_step(reads, labels, loss, test_loss, test_accuracy, model)
+        for batch, (reads, read_ids) in enumerate(test_input.take(test_steps), 1):
+            batch_predictions = testing_step(reads, read_ids, loss, test_loss, test_accuracy, model)
             if batch == 1:
-                all_labels = [labels]
+                all_read_ids = [read_ids]
                 all_predictions = batch_predictions
             else:
                 all_predictions = tf.concat([all_predictions, batch_predictions], 0)
-                all_labels = tf.concat([all_labels, [labels]], 1)
+                all_read_ids = tf.concat([all_read_ids, [read_ids]], 1)
 
         # get list of true species, predicted species and predicted probabilities
         all_predictions = all_predictions.numpy()
         pred_species = [np.argmax(j) for j in all_predictions]
         pred_probabilities = [np.amax(j) for j in all_predictions]
-        true_species = all_labels[0].numpy()
-        print(len(true_species), true_species)
-        true_one_hot = tf.one_hot(true_species, NUM_CLASSES)
+        all_read_ids = all_read_ids[0].numpy()
 
-        # save probabilities
-        np.save(os.path.join(output_dir, f'true-probs-{hvd.rank()}-{i}'), true_one_hot)
-        np.save(os.path.join(output_dir, f'pred-probs-{hvd.rank()}-{i}'), all_predictions)
-
-        # adjust the list of predicted and true species if necessary
+        # adjust the list of predicted species and read ids if necessary
         if len(pred_species) > num_reads:
             num_extra_reads = (test_steps*args.batch_size) - num_reads
             pred_species = pred_species[:-num_extra_reads]
-            true_species = true_species[:-num_extra_reads]
+            all_read_ids = all_read_ids[:-num_extra_reads]
 
-        # fill out confusion matrix and output probabilities
-        for j in range(len(true_species)):
-            cm[true_species[j], pred_species[j]] += 1
-            if true_species[j] == pred_species[j]:
-                num_correct_pred += 1
-                f_prob.write(f'correct\t{true_species[j]}\t{pred_species[j]}\t{pred_probabilities[j]}\n')
-            else:
-                num_incorrect_pred += 1
-                f_prob.write(f'incorrect\t{true_species[j]}\t{pred_species[j]}\t{pred_probabilities[j]}\n')
+        # fill out dictionary of bins and create summary file of predicted probabilities
+        gpu_bins = {label: [] for label in class_mapping.keys()} # key = species predicted, value = list of read ids
+        with open(os.path.join(output_dir, f'{gpu_test_files[i].split('.')[0]}-prob.tsv'), 'w') as out_f:
+            for j in range(num_reads):
+                gpu_bins[pred_species[j]].append(all_read_ids[j])
+                out_f.write(f'{pred_species[j]}\t{pred_probabilities[j]}\n')
 
-            # if hvd.rank() == 0:
-                # pred_classes += [np.argmax(i) for i in batch_pred.numpy().tolist()]
-                # true_classes += labels.numpy().tolist()
+        # get dictionary mapping read ids to labels
+        with open(os.path.join(args.tfrecords, gpu_read_ids_files[i]), 'r') as f:
+            content = f.readlines()
+            dict_read_ids = {content[j].rstrip().split('\t')[1]: content[j].rstrip().split('\t')[0] for j in range(len(content))}
 
-    f_prob.close()
+        # get reads
+        with open(os.path.join(args.input_fastq, f'{gpu_test_files[i].split('.')[0]}.fq'), 'r') as f:
+            content = f.readlines()
+            records = [''.join(content[j:j+4]) for j in range(0, len(content), 4)]
+            reads = {records[j].split('\n')[0].split(' ')[0]: records[j] for j in range(len(records))}
 
-    # write results to file
-    with open(os.path.join(output_dir, f'confusion-matrix-{hvd.rank()}.tsv'), 'w') as cm_f:
-        for i in range(NUM_CLASSES):
-            cm_f.write(f'\t{class_mapping[str(i)]}')
-        cm_f.write('\n')
-        for i in range(NUM_CLASSES):
-            cm_f.write(f'{class_mapping[str(i)]}')
-            for j in range(NUM_CLASSES):
-                cm_f.write(f'\t{cm[i,j]}')
-            cm_f.write('\n')
-
-    # compute average accuracy across all species
-    accuracy = round(float(num_correct_pred)/(num_correct_pred + num_incorrect_pred), 5)
+        # report species abundance and create bins
+        with open(os.path.join(output_dir, f'{gpu_test_files[i].split('.')[0]}-results.tsv'), 'w') as out_f:
+            for key, value in gpu_bins.items():
+                out_f.write(f'{key}\t{len(value)}\n')
+                # create fastq files from bins
+                with open(os.path.join(args.output_dir, f'bin-{key}', f'{gpu_test_files[i].split('.')[0]}.fq'), 'w') as out_fq:
+                    list_reads_in_bin = [reads[j] for j in value]
+                    out_fq.write(''.join(list_reads_in_bin))
 
     end = datetime.datetime.now()
     total_time = end - start
@@ -242,14 +229,11 @@ def main():
     minutes, seconds = divmod(seconds, 60)
 
     with open(os.path.join(output_dir, f'testing-summary-{hvd.rank()}.tsv'), 'w') as outfile:
-        outfile.write(f'testing set: {args.set_type}\nnumber of classes: {NUM_CLASSES}\nvector size: {VECTOR_SIZE}\nvocabulary size: {VOCAB_SIZE}\nembedding size: {EMBEDDING_SIZE}\ndropout rate: {DROPOUT_RATE}\nbatch size per gpu: {args.batch_size}\nnumber of gpus: {hvd.size()}\nGPU: {hvd.rank()}\nnumber of tfrecord files tested: {len(gpu_test_files)}\nnumber of reads per tfrecord file: 1000000\nnumber of reads in last tfrecord file: {args.num_reads - (len(test_files)*1000000)}\nnumber of reads tested: {num_reads_tested}\n')
-
+        outfile.write(f'batch size per gpu: {args.batch_size}\nnumber of gpus: {hvd.size()}\nGPU: {hvd.rank()}\nnumber of tfrecord files: {len(gpu_test_files)}\nnumber of reads classified: {num_reads_classified}\n')
         if args.ckpt:
             outfile.write(f'checkpoint saved at epoch: {args.epoch}')
         else:
             outfile.write(f'model saved at last epoch')
-
-        outfile.write(f'\naccuracy: {test_accuracy.result().numpy()*100}\naccuracy: {accuracy}\nloss: {test_loss.result().numpy()}\nrun time: {hours}:{minutes}:{seconds}:{total_time.microseconds}')
 
 
 if __name__ == "__main__":
