b02c3e7e9cfff07d17bff6a766b3b9be3874fd6f
Cecile Cres <cecilecres@Ceciles-Air.fios-router.home>
2022-01-23 18:42:06 -0500

    corrected format of testing summary

--- read_classifier_testing.py
+++ read_classifier_testing.py
@@ -204,7 +204,6 @@ def main():
         pred_species = [np.argmax(j) for j in all_predictions]
         pred_probabilities = [np.amax(j) for j in all_predictions]
         true_species = all_labels[0].numpy()
-        print(len(true_species), true_species)
         true_one_hot = tf.one_hot(true_species, NUM_CLASSES)
 
         # save probabilities
@@ -253,7 +252,7 @@ def main():
     minutes, seconds = divmod(seconds, 60)
 
     with open(os.path.join(output_dir, f'testing-summary-{hvd.rank()}.tsv'), 'w') as outfile:
-        outfile.write(f'testing set: {args.set_type}\nnumber of classes: {NUM_CLASSES}\nvector size: {VECTOR_SIZE}\nvocabulary size: {VOCAB_SIZE}\nembedding size: {EMBEDDING_SIZE}\ndropout rate: {DROPOUT_RATE}\nbatch size per gpu: {args.batch_size}\nnumber of gpus: {hvd.size()}\nGPU: {hvd.rank()}\nnumber of tfrecord files tested: {len(gpu_test_files)}\nnumber of reads per tfrecord file: 1000000\nnumber of reads in last tfrecord file: {args.num_reads - (len(test_files)*1000000)}\nnumber of reads tested: {num_reads_tested}\n')
+        outfile.write(f'testing set: {args.set_type}\nnumber of classes: {NUM_CLASSES}\nvector size: {VECTOR_SIZE}\nvocabulary size: {VOCAB_SIZE}\nembedding size: {EMBEDDING_SIZE}\ndropout rate: {DROPOUT_RATE}\nbatch size per gpu: {args.batch_size}\nnumber of gpus: {hvd.size()}\nGPU: {hvd.rank()}\nnumber of tfrecord files tested: {len(gpu_test_files)}\nnumber of reads tested: {num_reads_tested}\n')
 
         if args.ckpt:
             outfile.write(f'checkpoint saved at epoch: {args.epoch}')
