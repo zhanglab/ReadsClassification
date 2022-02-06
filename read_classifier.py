666aaa68a0eff0a3f2376bdea0aceaef39874164
Cecile Cres <cecilecres@Ceciles-Air.fios-router.home>
2022-02-06 13:33:58 -0500

    corrected full read id

--- read_classifier.py
+++ read_classifier.py
@@ -219,13 +219,13 @@ def main():
         # get dictionary mapping read ids to labels
         with open(os.path.join(args.tfrecords, gpu_read_ids_files[i]), 'r') as f:
             content = f.readlines()
-            dict_read_ids = {content[j].rstrip().split('\t')[1]: content[j].rstrip().split('\t')[0] for j in range(len(content))}
+            dict_read_ids = {content[j].rstrip().split('\t')[1]: '@' + content[j].rstrip().split('\t')[0] for j in range(len(content))}
 
         # get reads
         with gzip.open(os.path.join(args.fq_files, f'{gpu_test_files[i].split("/")[-1].split(".")[0]}.fastq.gz'), 'rt') as f:
             content = f.readlines()
             records = [''.join(content[j:j+4]) for j in range(0, len(content), 4)]
-            reads = {'@' + records[j].split('\n')[0]: records[j] for j in range(len(records))}
+            reads = {records[j].split('\n')[0]: records[j] for j in range(len(records))}
 
         # report species abundance and create bins
         with open(os.path.join(args.output_dir, f'{gpu_test_files[i].split("/")[-1].split(".")[0]}-results.tsv'), 'w') as out_f:
