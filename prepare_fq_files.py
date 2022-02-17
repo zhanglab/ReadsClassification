ac8c7a5a0fb17fa1863d6ddbca5f27d172246643
Cecile Cres <cecilecres@Ceciles-Air.fios-router.home>
2022-02-16 20:14:10 -0500

    added number of reads as command line argument

--- prepare_fq_files.py
+++ prepare_fq_files.py
@@ -2,7 +2,7 @@ import sys
 import os
 import math
 import gzip
-import argparse
+
 
 def split_fq_file(reads, out_filename, n_reads):
     # compute number of output fq files
@@ -21,17 +21,15 @@ def get_reads(fq_file):
     return reads
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--fq_file', help="Path to the input fastq file")
-    parser.add_argument('--output_dir', help="Path to the output directory")
-    parser.add_argument('--n_reads', default=500000, type=int, help="number of reads per fastq file")
-    args = parser.parse_args()
+    fq_file = sys.argv[1]
+    output_dir = sys.argv[2]
+    n_reads = sys.argv[3]
     # get list of reads
-    reads = get_reads(args.fq_file)
+    reads = get_reads(fq_file)
     # split list of fq file if
-    if len(reads) > args.n_reads:
-        out_filename = os.path.join(args.output_dir, ''.join(fq_file.split('/')[-1].split('.')[0]))
-        split_fq_file(reads, out_filename, args.n_reads)
+    if len(reads) > n_reads:
+        out_filename = os.path.join(output_dir, ''.join(fq_file.split('/')[-1].split('.')[0]))
+        split_fq_file(reads, out_filename, n_reads)
 
 if __name__ == "__main__":
     main()
