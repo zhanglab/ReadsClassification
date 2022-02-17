f4209ff76946c4fb0da4d05b9072487fd3b6588a
Cecile Cres <cecilecres@Ceciles-Air.fios-router.home>
2022-02-16 19:58:38 -0500

    added number of reads as command line argument

--- prepare_fq_files.py
+++ prepare_fq_files.py
@@ -2,13 +2,14 @@ import sys
 import os
 import math
 import gzip
+import argparse
 
-def split_fq_file(reads, out_filename):
+def split_fq_file(reads, out_filename, n_reads):
     # compute number of output fq files
-    n_files = math.ceil(len(reads)/500000)
+    n_files = math.ceil(len(reads)/n_reads)
     for i in range(n_files):
-        start = i*500000
-        end = (i*500000) + 500000 if i < n_files - 1 else (i*500000) + len(reads)
+        start = i*n_reads
+        end = (i*n_reads) + n_reads if i < n_files - 1 else (i*n_reads) + len(reads)
         with gzip.open('-'.join([out_filename, f'{i}.fastq.gz']), 'wt') as outfile:
             outfile.write(''.join(reads[start:end]))
 
@@ -20,14 +21,17 @@ def get_reads(fq_file):
     return reads
 
 def main():
-    fq_file = sys.argv[1]
-    output_dir = sys.argv[2]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fq_file', help="Path to the input fastq file")
+    parser.add_argument('--output_dir', help="Path to the output directory")
+    parser.add_argument('--n_reads', default=500000, type=int, help="number of reads per fastq file")
+    args = parser.parse_args()
     # get list of reads
-    reads = get_reads(fq_file)
+    reads = get_reads(args.fq_file)
     # split list of fq file if
-    if len(reads) > 500000:
-        out_filename = os.path.join(output_dir, ''.join(fq_file.split('/')[-1].split('.')[0]))
-        split_fq_file(reads, out_filename)
+    if len(reads) > args.n_reads:
+        out_filename = os.path.join(args.output_dir, ''.join(fq_file.split('/')[-1].split('.')[0]))
+        split_fq_file(reads, out_filename, args.n_reads)
 
 if __name__ == "__main__":
     main()
