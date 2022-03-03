971c51edcf719b59c7ce1e08294489806b4c98c6
Cecile Cres <cecilecres@Ceciles-Air.fios-router.home>
2022-03-03 14:53:12 -0500

    adapted code to graph execution

--- read_classifier_testing.py
+++ read_classifier_testing.py
@@ -210,8 +210,10 @@ def main():
         all_pred_sp, all_prob_sp, all_labels = input_test(args.batch_size, test_steps, test_input, model, loss, test_loss, test_accuracy)
 
         tf.config.run_functions_eagerly(True)
+        print(tf.executing_eagerly())
         summarize_results(all_pred_sp, all_prob_sp, all_labels)
         tf.config.run_functions_eagerly(False)
+        print(tf.executing_eagerly())
         # create empty arrays to store the predicted and true values
         # all_predictions = tf.zeros([args.batch_size, NUM_CLASSES], dtype=tf.dtypes.float32, name=None)
         # all_pred_sp = tf.zeros([args.batch_size], dtype=tf.dtypes.float32, name=None)
