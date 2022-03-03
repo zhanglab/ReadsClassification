973486ef35b4160d58c8713a552869285f84f37c
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2022-03-03 12:30:12 -0500

    adapted code to graph execution

--- read_classifier_testing.py
+++ read_classifier_testing.py
@@ -209,6 +209,7 @@ def main():
 
         test_input = test_preprocessor.get_device_dataset()
         input_test(args.batch_size, test_steps, test_input, model, loss, test_loss, test_accuracy)
+        break
 
         # create empty arrays to store the predicted and true values
         # all_predictions = tf.zeros([args.batch_size, NUM_CLASSES], dtype=tf.dtypes.float32, name=None)
