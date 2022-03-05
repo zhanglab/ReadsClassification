07217c2558c7abc30c4e87174f4db9f339d93583
Cecile Cres <cecilecres@Ceciles-Air.fios-router.home>
2022-03-05 09:11:56 -0500

    adapted code to graph execution

--- read_classifier_testing.py
+++ read_classifier_testing.py
@@ -210,22 +210,22 @@ def main():
         all_pred_sp, all_prob_sp, all_labels = input_test(args.batch_size, test_steps, test_input, model, loss, test_loss, test_accuracy)
 
         # metadata can't be found
-        # ds_tensors_pred = tf.data.Dataset.from_tensors(all_pred_sp)
-        # tf.data.experimental.save(ds_tensors_pred, os.path.join(args.output_dir, f'{gpu_test_files[i].split("/")[-1].split(".")[0]}-pred-tensors'), compression='GZIP')
+        ds_tensors_pred = tf.data.Dataset.from_tensors(all_pred_sp)
+        tf.data.experimental.save(ds_tensors_pred, os.path.join(args.output_dir, f'{gpu_test_files[i].split("/")[-1].split(".")[0]}-pred-tensors'), compression='GZIP')
+        
+        with open(os.path.join(args.output_dir, f'{gpu_test_files[i].split("/")[-1].split(".")[0]}-pred-tensors', 'element_spec'), 'wb') as out_:  # also save the element_spec to disk for future loading
+            pickle.dump(ds_tensors_pred.element_spec, out_)
+
+        # ds = tf.data.Dataset.from_tensors(all_pred_sp)
+        # ds = ds.map(tf.io.serialize_tensor)
+        # writer = tf.data.experimental.TFRecordWriter(os.path.join(args.output_dir, f'{gpu_test_files[i].split("/")[-1].split(".")[0]}-pred-tensors.tfrecord'))
+        # writer.write(ds)
         #
-        # with open(os.path.join(args.output_dir, f'{gpu_test_files[i].split("/")[-1].split(".")[0]}-pred-tensors', 'element_spec'), 'wb') as out_:  # also save the element_spec to disk for future loading
-        #     pickle.dump(ds_tensors_pred.element_spec, out_)
-
-        ds = tf.data.Dataset.from_tensors(all_pred_sp)
-        ds = ds.map(tf.io.serialize_tensor)
-        writer = tf.data.experimental.TFRecordWriter(os.path.join(args.output_dir, f'{gpu_test_files[i].split("/")[-1].split(".")[0]}-pred-tensors.tfrecord'))
-        writer.write(ds)
-
-        init = tf.compat.v1.global_variables_initializer()
-
-        sess = tf.compat.v1.Session()
-        sess.run(init)
-        sess.run(writer)
+        # init = tf.compat.v1.global_variables_initializer()
+        #
+        # sess = tf.compat.v1.Session()
+        # sess.run(init)
+        # sess.run(writer)
 
         # pred_string = tf.strings.format("{}", (all_pred_sp))
         # prob_string = tf.strings.format("{}", (all_prob_sp))
