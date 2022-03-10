55aa6d37010a425e77a23b1f78ae9c5b8f14e824
Cecile Cres <cecilecres@Ceciles-Air.fios-router.home>
2022-03-09 21:51:31 -0500

    implemented loading of npy files

--- summarize_results.py
+++ summarize_results.py
@@ -12,13 +12,22 @@ def get_results(list_tsv_files):
     pred_species = []
     true_species = []
     probs = []
-    for file_ in list_tsv_files:
-        df = pd.read_csv(file_, header=None)
+    for in_file in list_tsv_files:
+        df = pd.read_csv(in_file, header=None)
         true_species += df.iloc[:,0].tolist()
         pred_species += df.iloc[:,1].tolist()
         probs += df.iloc[:,2].tolist()
 
     return pred_species, true_species, probs
+
+def load_prob_files(list_prob_files):
+    probs = []
+    for in_file in list_prob_files:
+        arr = np.load(in_file)
+        probs += arr.tolist()
+    print(f'# files: {len(list_prob_files)}\t# reads: {len(probs)}')
+    return probs
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -31,36 +40,38 @@ def main():
         species_mapping_dict = json.load(f)
 
     list_tsv_files = sorted(glob.glob(os.path.join(args.input_dir, 'tmp', '*.tsv')))
+    list_prob_files = sorted(glob.glob(os.path.join(args.input_dir, 'tmp', '*.npy')))
 
     if args.data_type == 'test':
         # get predictions and ground truth at species level
         start_time = time.time()
         pred_species, true_species, probs = get_results(list_tsv_files)
+        all_probs = load_prob_files(list_prob_files)
         end_time = time.time()
         print(end_time - start_time)
         # get confusion matrix
         cm, accuracy = get_cm(true_species, pred_species, species_mapping_dict, 'species')
         # get decision thresholds
-        ROCcurve(args, true_species, probs, species_mapping_dict, set(true_species), 'species')
+        ROCcurve(args, true_species, all_probs, species_mapping_dict, set(true_species), 'species')
         # get precision and recall
         get_metrics(args, cm, species_mapping_dict, set(true_species), 'species')
         # analyze results at higher taxonomic levels
-        for r in ['genus', 'family', 'order', 'class']:
-            # load dictionary mapping species labels to other ranks labels
-            with open(os.path.join(args.rank_mapping_dir, f'{r}_species_labels.json')) as f_json:
-                rank_species_mapping = json.load(f_json)
-            # get vectors of predicted and true labels at given rank
-            rank_pred_taxa = [rank_species_mapping[str(i)] for i in pred_species]
-            rank_true_taxa = [rank_species_mapping[str(i)] for i in true_species]
-            # load dictionary mapping labels to taxa at given rank
-            with open(os.path.join(args.rank_mapping_dir, f'{r}_labels.json')) as f_json:
-                rank_mapping_dict = json.load(f_json)
-            # get confusion matrix
-            cm, accuracy = get_cm(rank_true_taxa, rank_pred_taxa, rank_mapping_dict, r)
-            # get decision thresholds
-            ROCcurve(args, rank_true_taxa, probs, rank_mapping_dict, set(rank_true_taxa), r)
-            # get precision and recall
-            get_metrics(args, cm, rank_mapping_dict, set(rank_true_taxa), r)
+        # for r in ['genus', 'family', 'order', 'class']:
+        #     # load dictionary mapping species labels to other ranks labels
+        #     with open(os.path.join(args.rank_mapping_dir, f'{r}_species_labels.json')) as f_json:
+        #         rank_species_mapping = json.load(f_json)
+        #     # get vectors of predicted and true labels at given rank
+        #     rank_pred_taxa = [rank_species_mapping[str(i)] for i in pred_species]
+        #     rank_true_taxa = [rank_species_mapping[str(i)] for i in true_species]
+        #     # load dictionary mapping labels to taxa at given rank
+        #     with open(os.path.join(args.rank_mapping_dir, f'{r}_labels.json')) as f_json:
+        #         rank_mapping_dict = json.load(f_json)
+        #     # get confusion matrix
+        #     cm, accuracy = get_cm(rank_true_taxa, rank_pred_taxa, rank_mapping_dict, r)
+        #     # get decision thresholds
+        #     ROCcurve(args, rank_true_taxa, probs, rank_mapping_dict, set(rank_true_taxa), r)
+        #     # get precision and recall
+        #     get_metrics(args, cm, rank_mapping_dict, set(rank_true_taxa), r)
 
 
 
