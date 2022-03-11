045327ce0ff021276826f23313797c0a70a838c5
Cecile Cres <cecilecres@216.157.20.172.s.wireless.uri.edu>
2022-03-11 16:11:33 -0500

    uncommented higher taxonomic groups

--- summarize_results.py
+++ summarize_results.py
@@ -56,22 +56,22 @@ def main():
         # get precision and recall
         get_metrics(args, cm, species_mapping_dict, set(true_species), 'species')
         # analyze results at higher taxonomic levels
-        # for r in ['genus', 'family', 'order', 'class']:
-        #     # load dictionary mapping species labels to other ranks labels
-        #     with open(os.path.join(args.rank_mapping_dir, f'{r}_species_labels.json')) as f_json:
-        #         rank_species_mapping = json.load(f_json)
-        #     # get vectors of predicted and true labels at given rank
-        #     rank_pred_taxa = [rank_species_mapping[str(i)] for i in pred_species]
-        #     rank_true_taxa = [rank_species_mapping[str(i)] for i in true_species]
-        #     # load dictionary mapping labels to taxa at given rank
-        #     with open(os.path.join(args.rank_mapping_dir, f'{r}_labels.json')) as f_json:
-        #         rank_mapping_dict = json.load(f_json)
-        #     # get confusion matrix
-        #     cm, accuracy = get_cm(rank_true_taxa, rank_pred_taxa, rank_mapping_dict, r)
-        #     # get decision thresholds
-        #     ROCcurve(args, rank_true_taxa, probs, rank_mapping_dict, set(rank_true_taxa), r)
-        #     # get precision and recall
-        #     get_metrics(args, cm, rank_mapping_dict, set(rank_true_taxa), r)
+        for r in ['genus', 'family', 'order', 'class']:
+            # load dictionary mapping species labels to other ranks labels
+            with open(os.path.join(args.rank_mapping_dir, f'{r}_species_labels.json')) as f_json:
+                rank_species_mapping = json.load(f_json)
+            # get vectors of predicted and true labels at given rank
+            rank_pred_taxa = [rank_species_mapping[str(i)] for i in pred_species]
+            rank_true_taxa = [rank_species_mapping[str(i)] for i in true_species]
+            # load dictionary mapping labels to taxa at given rank
+            with open(os.path.join(args.rank_mapping_dir, f'{r}_labels.json')) as f_json:
+                rank_mapping_dict = json.load(f_json)
+            # get confusion matrix
+            cm, accuracy = get_cm(rank_true_taxa, rank_pred_taxa, rank_mapping_dict, r)
+            # get decision thresholds
+            ROCcurve(args, rank_true_taxa, s_probs, rank_mapping_dict, set(rank_true_taxa), r)
+            # get precision and recall
+            get_metrics(args, cm, rank_mapping_dict, set(rank_true_taxa), r)
 
 if __name__ == "__main__":
     main()
