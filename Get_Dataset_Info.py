a1ff38c14e7410e3eef6e4f04b34c512ab150452
eric_rangel <ejrangel2@alaska.edi>
2022-06-22 12:44:09 -0400

    Statistical Analysis functions

---
+++ Get_Dataset_Info.py
@@ -0,0 +1,74 @@
+from collections import defaultdict
+import sys
+import pandas as pd
+import os
+import multiprocessing as mp
+import math
+
+# This function creates a dataframe from a tsv file. Then, we iterate through the dataframe to make a dictionary.
+# The species labels are the keys and the values are a list filled with respective genome associations.
+
+
+def create_df(path):
+    count = 0  # variable used to iterate through the dataframe
+    df = pd.read_csv(path, sep='\t', header=None)  # converts the tsv file to a dataframe
+    NCBI_dict = defaultdict(list)  # makes the default dictionary value a list
+
+# iterates through the dataframe to create the dictionary
+
+    for key in df[1]:
+        NCBI_dict[key].append(df[0][count])
+        count += 1
+
+    return NCBI_dict
+
+# Finds the length of each list in a dictionary and associates this new integer to the key of the previous list
+
+
+def create_dict_count(dict_list):
+    temp = {}
+    for key in dict_list:
+        temp[key] = len(dict_list[key])
+
+    return temp
+
+# Counts the number of reads associated with each species label and makes a new dictionary with this value
+
+
+def num_reads(path, genome_dict, process_id):
+    reads_dict = defaultdict(int)
+    with open(f'results-process-id-{process_id}', 'w') as out_f:
+        for label, genome_list in genome_dict.items():
+            out_f.write(f'{label}\t{len(genome_list)}\t')
+            for genome_fq in genome_list:
+                with open(os.path.join(path, (genome_fq + '.fq')), 'r') as f:
+                    content = f.readlines()
+                records = [''.join(content[i:i + 4]) for i in range(0, len(content), 4)]
+                reads_dict[label] += math.ceil(len(records) * .70)  # Only 70% of the reads are used for testing
+            out_f.write(f'{reads_dict[label]}\n')
+
+
+def split_dict(input_dict: dict, num_parts: int) -> list:
+    list_len: int = len(input_dict)
+    return [dict(list(input_dict.items())[i * list_len // num_parts:(i + 1) * list_len // num_parts])
+            for i in range(num_parts)]
+
+
+def main():
+
+    NCBI_training_path = sys.argv[1]
+    fq_path = sys.argv[2]
+    labels_dict = create_df(NCBI_training_path)
+    list_label_dict = split_dict(labels_dict, mp.cpu_count())
+
+    processes = [mp.Process(target=num_reads, args=(fq_path, list_label_dict[i], i)) for i in
+                 range(len(list_label_dict))]
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
+
+
+if __name__ == '__main__':
+    main()
+
