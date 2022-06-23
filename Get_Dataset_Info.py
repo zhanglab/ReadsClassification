0a37495e608679b9aaf6dba1727d9f834df97b6b
Cecile Cres <cecilecres@Ceciles-Air.fios-router.home>
2022-06-23 09:00:02 -0400

    added command line arguments

--- Get_Dataset_Info.py
+++ Get_Dataset_Info.py
@@ -45,7 +45,7 @@ def num_reads(path, genome_dict, species_dict, output_dir, process_id):
                 with open(os.path.join(path, (genome_fq + '.fq')), 'r') as f:
                     content = f.readlines()
                 records = [''.join(content[i:i + 4]) for i in range(0, len(content), 4)]
-                reads_dict[label] += math.ceil(len(records) * .70)  # Only 70% of the reads are used for testing
+                reads_dict[label] += math.ceil(len(records) * .70)  # Only 70% of the reads are used for training
             out_f.write(f'{reads_dict[label]}\n')
 
 # split dictionary into sub dictionaries with one dictionary per process
@@ -57,21 +57,24 @@ def split_dict(input_dict: dict, num_parts: int) -> list:
 
 def main():
 
-    NCBI_training_path = sys.argv[1]
-    fq_path = sys.argv[2]
-    json_path = sys.argv[3] # path to directory containing species_labels.json dictionary
-    output_dir = sys.argv[4]
+    NCBI_info_path = sys.argv[1] # path to file containing list of genomes mapped to species labels
+    fq_path = sys.argv[2] # path to directory containing fastq files in training or testing set
+    json_path = sys.argv[3] # path to species_labels_full_taxonomy.json dictionary
+    output_dir = sys.argv[4] # path to output directory
+    dataset_type = sys.argv[5] # testing or training datasets
+    level_analysis = sys.argv[6] # label or genome --> # reads are reported per genome or per label
 
     species_dict = load_json_dict(json_path)
-    labels_dict = create_df(NCBI_training_path)
-    list_label_dict = split_dict(labels_dict, mp.cpu_count())
+    labels_dict = create_df(NCBI_info_path)
+    print(labels_dict)
+    # list_label_dict = split_dict(labels_dict, mp.cpu_count())
 
-    processes = [mp.Process(target=num_reads, args=(fq_path, list_label_dict[i], species_dict, output_dir, i)) for i in
-                 range(len(list_label_dict))]
-    for p in processes:
-        p.start()
-    for p in processes:
-        p.join()
+    # processes = [mp.Process(target=num_reads, args=(fq_path, list_label_dict[i], species_dict, output_dir, i)) for i in
+    #              range(len(list_label_dict))]
+    # for p in processes:
+    #     p.start()
+    # for p in processes:
+    #     p.join()
 
 
 if __name__ == '__main__':
