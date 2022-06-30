86e52cc765a51037c639ff45d6f086f3fdad5b88
eric_rangel <ejrangel2@alaska.edi>
2022-06-30 15:48:53 -0400

    added count_excel_reads.py

--- count_excel_reads.py
+++ count_excel_reads.py
@@ -4,19 +4,21 @@ import pandas as pd
 import numpy as np
 
 
-def count_reads_excel(path):
+def count_reads_excel(path, rank):
     total_reads = 0
-    excel_list = excel_files = sorted(glob.glob(os.path.join(f'*-confusion-matrix.xlsx')))
+    excel_list = sorted(glob.glob(os.path.join(path, f'*-confusion-matrix.xlsx')))
     for file in excel_list:
         df = pd.read_excel(file, index_col=0, sheet_name=None)
-        total_reads += df['species'].to_numpy().sum()
+        total_reads += df[rank].to_numpy().sum()
     return total_reads
 
 
 def main():
     path = "/data/zhanglab/eric_rangel/centrifuge_results"
-    total = count_reads_excel(path)
-    print(total)
+    ranks = ["species", "genus", "family", "order", "class", "phylum"]
+    for r in ranks:
+        total = count_reads_excel(path, r)
+        print(f'total for {r} level: {total}')
 
 
 if __name__ == "__main__":
