07f39748ab3be9b90edacc7e8befe5ec16caa3f9
Cecile Cres <cecilecres@Ceciles-MacBook-Air.local>
2022-07-04 00:32:09 -0600

    run code in parallel

--- split-reads-testing-set.py
+++ split-reads-testing-set.py
@@ -14,7 +14,7 @@ def get_reads(fq_files, fw_reads, rv_reads):
                 elif r.split('\n')[0][-1] == '2':
                     rv_reads[r.split('\n')[0][:-2]] = r
 
-def write_reads_to_file(reads, fw_reads, rv_reads, type_of_reads):
+def write_paired_reads_to_file(reads, fw_reads, rv_reads):
     num_reads_per_set = 500000
     total_num_reads = 0
     for i in range(0, len(reads), num_reads_per_set):
@@ -28,13 +28,28 @@ def write_reads_to_file(reads, fw_reads, rv_reads, type_of_reads):
             out_f.write(''.join(subset_rv_reads))
     print(f'total # reads: {total_num_reads}')
 
+def write_unpaired_reads_to_file(reads, fw_reads, rv_reads):
+    num_reads_per_set = 500000
+    total_num_reads = 0
+    for i in range(0, len(reads), num_reads_per_set):
+        subset_reads = []
+        for r in reads[i:i+num_reads_per_set]:
+            if r in fw_reads:
+                subset_reads.append(fw_reads[r])
+            elif r in rv_reads:
+                subset_reads.append(rv_reads[r])
+        print(f'subset: {i}\t{len(subset_reads)}')
+        total_num_reads += len(subset_reads)
+        with open(os.path.join(output_dir, f'unpaired-updated-testing-set-{i}-reads.fq'), 'w') as out_f:
+            out_f.write(''.join(subset_fw_reads))
+    print(f'total # reads: {total_num_reads}')
 
 if __name__ == "__main__":
     input_dir = sys.argv[1]
     output_dir = sys.argv[2]
 
     list_fq_files = sorted(glob.glob(os.path.join(input_dir, 'updated-testing-set-*-reads.fq')))
-    chunk_size = 10
+    chunk_size = 20
     set_fq_files = [list_fq_files[i:i+chunk_size] for i in range(0, len(list_fq_files), chunk_size)]
     print(f'{chunk_size}\t{len(set_fq_files)}\t{len(list_fq_files)}')
     with mp.Manager() as manager:
@@ -52,5 +67,5 @@ if __name__ == "__main__":
         paired_reads = list(set(fw_reads.keys()).intersection(set(rv_reads.keys())))
         print(f'# unpaired reads: {len(unpaired_reads)}\t# paired reads: {len(paired_reads)}')
 
-        write_reads_to_file(unpaired_reads, fw_reads, rv_reads, 'unpaired')
-        write_reads_to_file(paired_reads, fw_reads, rv_reads, 'unpaired')
+        write_unpaired_reads_to_file(unpaired_reads, fw_reads, rv_reads)
+        write_paired_reads_to_file(paired_reads, fw_reads, rv_reads)
